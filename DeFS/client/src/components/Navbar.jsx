f4fbbe18a4d734e541df631479d9e830782fa943
Darshan Makwana <darshanmakwana412@gmail.com>
2022-06-25 22:22:57 +0530

    checkpoint 2

---
+++ DeFS/client/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = ()=> {
+    return (
+        <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+            This is the Navigation Bar
+      </nav>
+    )
+}
+
+export default Navbar;
